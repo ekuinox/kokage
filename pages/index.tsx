36bc76e52e1fb728da8c5dd25ccaf1e2ecfd098d
ekuinox <depkey@me.com>
2023-04-03 00:31:33 +0900

    これでいいやろ！

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { getServerSession } from 'next-auth';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { Container } from '@mantine/core';
+import { Anchor, Card, Container, Stack, Text, Title } from '@mantine/core';
 import { Head } from '@/components/Head';
 import { Header } from '@/components/Header';
 import { UserTopTracks, UserTopTracksProps } from '@/components/UserTopTracks';
@@ -15,11 +17,33 @@ export default function Home({
       <Head />
       <main>
         <Header />
-        {userTopTracksProps && (
-          <Container my="sm">
+        <Container my="sm">
+          {(userTopTracksProps && (
             <UserTopTracks {...userTopTracksProps} />
-          </Container>
-        )}
+          )) || (
+            <Stack mx="auto" align="center">
+              <Title fz="lg">よく聴く曲をシェアするやつ</Title>
+              <Text>
+                Spotifyの連携を使ってよく聴いた曲をリストで出すやつです。
+                <Anchor component={Link} href="/api/auth/signin">
+                  ログイン
+                </Anchor>
+                してシェアしてみてください！
+              </Text>
+              <Card shadow="sm" padding="xs" mx="xl" radius="md" withBorder>
+                <Card.Section>
+                  <Image
+                    src="/capture-sample.png"
+                    alt="サンプル画像"
+                    width={390}
+                    height={617}
+                  />
+                </Card.Section>
+              </Card>
+              <Text>こんな感じのが出ます！</Text>
+            </Stack>
+          )}
+        </Container>
       </main>
     </>
   );
