cdbcc319c1e6ff489d139b114905af48c08c2381
ekuinox <depkey@me.com>
2023-04-04 21:11:13 +0900

    frameBorderをやめる

--- components/IframedTracks.tsx
+++ components/IframedTracks.tsx
@@ -11,12 +11,12 @@ export const IframedTracks = ({ tracks }: IframedTracksProps) => {
         <iframe
           key={track.id}
           src={`https://open.spotify.com/embed/track/${track.id}`}
+          style={{ borderStyle: 'none' }}
           height="80"
-          frameBorder="0"
           allowFullScreen
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-        ></iframe>
+        />
       ))}
     </>
   );
