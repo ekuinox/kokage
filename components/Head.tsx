6c492ed58498d052740e1942eab47cd3619fab49
ekuinox <depkey@me.com>
2023-04-04 21:32:04 +0900

    試しにOGP

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,6 +6,14 @@ export const Head = () => {
       <title>よく聴く曲をシェアするやつ</title>
       <meta name="description" content="よく聴く曲をシェアするやつ" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:url" content={'https://kokage.ekuinox.dev'} />
+      <meta property="og:title" content={'よく聴く曲をシェアするやつ'} />
+      <meta property="og:site_name" content={'よく聴く曲をシェアするやつ'} />
+      <meta property="og:description" content={'よく聴く曲をシェアするやつ'} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={'https://github.com/ekuinox.png'} />
+      <meta property="og:image:width" content={'400'} />
+      <meta property="og:image:height" content={'400'} />
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
